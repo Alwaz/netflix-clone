c8bace720efe87a7ae81b3a2e82866ffbc5ce1c8
Alwaz <alwazkazi3@gmail.com>
2022-02-24 08:31:47 -0800

    fixed typo.

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// base url tp send reques to database and whenever this instance is called it will append the value to url.
+// base url to send reques to database and whenever this instance is called it will append the value to url.
 const instance = axios.create({
   baseURL: `https://api.themoviedb.org/3`,
 });
